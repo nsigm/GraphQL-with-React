340bcd7ee12bfbf3ae3ff82c636b1bcd62819cd6
Nils <nils@sld.gs>
2021-07-16 15:19:35 +0200

    add: error handling of graphql errors, signup component

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -5,18 +5,32 @@ import { graphql } from "react-apollo";
 import query from "../queries/CurrentUser";
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query }],
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((err) => err.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div className="container">
         <h3>Login</h3>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
---
+++ client/components/SignupForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import AuthForm from "./AuthForm";
+import { graphql } from "react-apollo";
+import mutation from "../mutations/Signup";
+import query from "../queries/CurrentUser";
+
+class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(err => err.message);
+      this.setState({ errors });
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h3>Signup</h3>
+        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default graphql(mutation)(SignupForm);
