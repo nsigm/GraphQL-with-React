67e72836d3310e6447b588d71cc3b897ff75ed97
Nils <nils@sld.gs>
2021-08-19 11:17:35 +0200

    fix: auth flow, error handling for login and signup, add requireAuth HOC

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -3,6 +3,7 @@ import AuthForm from "./AuthForm";
 import { graphql } from "react-apollo";
 import mutation from "../mutations/Signup";
 import query from "../queries/CurrentUser";
+import { hashHistory } from "react-router";
 
 class SignupForm extends Component {
   constructor(props) {
@@ -11,24 +12,35 @@ class SignupForm extends Component {
     this.state = { errors: [] };
   }
 
+  componentWillUpdate(nextProps) {
+    if (nextProps.data.user && !this.props.data.user) {
+      hashHistory.push("/dashboard");
+    }
+  }
+
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query }]
-    }).catch(res => {
-      const errors = res.graphQLErrors.map(err => err.message);
-      this.setState({ errors });
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((err) => err.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div className="container">
         <h3>Signup</h3>
-        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(graphql(mutation)(SignupForm));
